088ffe3df2712ae3c54d70377851eb47d481bc65
hughwin <hugh.winchester@gmail.com>
2020-02-23 17:46:01 +0000

    Better organisation of code

--- src/main/java/se/controller/MenuActionListener.java
+++ src/main/java/se/controller/MenuActionListener.java
@@ -1,12 +1,20 @@
 package se.controller;
 
 import se.appview.AppMenu;
+import se.appview.AppView;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
 public class MenuActionListener implements ActionListener {
+
+    private final AppView appView;
+
+
+    public MenuActionListener(){
+        this.appView = new AppView(this);
+    }
 
 
     @Override
@@ -17,11 +25,13 @@ public class MenuActionListener implements ActionListener {
         switch (command) {
 
             case AppMenu.LOAD_TEACHER_PAGE:
-                //showTeacherView();
+
+                appView.getCardLayout().show(appView.getMainPanel(), "1");
                 System.out.println(command + " clicked");
                 break;
             case AppMenu.LOAD_COURSE_PAGE:
                 //showCoursesView();
+                appView.getCardLayout().show(appView.getMainPanel(), "2");
                 System.out.println(command + " clicked");
                 break;
 
