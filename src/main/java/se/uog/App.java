436339b39329914df0622ddc27767c10ebd0d3d2
hughwin <hugh.winchester@gmail.com>
2020-02-25 13:34:20 +0000

    Added command line check to enable logger.

--- src/main/java/se/uog/App.java
+++ src/main/java/se/uog/App.java
@@ -1,20 +1,24 @@
 package se.uog;
 
 import se.uog.controller.AppController;
+import se.uog.logger.Logger;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public class App {
+
+    private static String LOGGER_FLAG = "-t";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        boolean writeGameLogsToFile = false; // Should we write game logs to file?
+        if (args.length > 0) {
+            if (args[0].equals(LOGGER_FLAG)) {
+                System.out.println("Logger enabled");
+                Logger.getInstance().enable();
+                Logger.getInstance().logToFileIfEnabled("Logger enabled.");
+            }
+        }
+
         new AppController();
     }
 }
