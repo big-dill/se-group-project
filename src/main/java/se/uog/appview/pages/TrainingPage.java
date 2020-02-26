339e24eb168c84bb5670a23591f8ebc6bc6003f1
Julia Saari <mariajulia.saari@gmail.com>
2020-02-26 11:58:56 +0000

    modified TrainingPage

--- src/main/java/se/uog/appview/pages/TrainingPage.java
+++ src/main/java/se/uog/appview/pages/TrainingPage.java
@@ -2,19 +2,17 @@ package se.uog.appview.pages;
 
 import se.uog.table.JObjectTable;
 import se.uog.table.ObjectTableModel;
+
+import javax.swing.JPanel;
+
 import se.uog.model.Training;
 
-public class TrainingPage extends PageView implements TablePageView<Training> {
+public class TrainingPage extends JPanel implements TablePageView<Training> {
     private JObjectTable<Training> table;
 
     public TrainingPage (ObjectTableModel<Training> tableModel){
         table = new JObjectTable<>(tableModel);
         add(table);
-    }
-
-    @Override
-    public String getName() {
-        return "Training";
     }
 
     @Override
