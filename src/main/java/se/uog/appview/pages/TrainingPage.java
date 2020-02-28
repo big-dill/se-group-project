8a20dc48d5bd99aae7ca6299ad8edc2091597e5f
hughwin <hugh.winchester@gmail.com>
2020-02-28 16:09:39 +0000

    Added propertyChangeListener to change view.

--- src/main/java/se/uog/appview/pages/TrainingPage.java
+++ src/main/java/se/uog/appview/pages/TrainingPage.java
@@ -1,18 +1,38 @@
 package se.uog.appview.pages;
 
+import se.uog.model.UserEnum;
+import se.uog.model.UserType;
 import se.uog.table.JObjectTable;
 import se.uog.table.ObjectTableModel;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import se.uog.model.Training;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class TrainingPage extends JPanel implements TablePageView<Training> {
     private JObjectTable<Training> table;
 
     public TrainingPage (ObjectTableModel<Training> tableModel){
         table = new JObjectTable<>(tableModel);
+        UserType ue = UserType.getInstance();
+        JLabel jl = new JLabel(String.valueOf(ue.toString()));
+        add(jl);
+
         add(table);
+
+
+
+        UserType.getInstance().addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+                System.out.println(ue.getUserEnum());
+                jl.setText(String.valueOf(ue));
+            }
+        });
+
     }
 
     @Override
--- src/main/java/se/uog/model/UserType.java
+++ src/main/java/se/uog/model/UserType.java
@@ -1,20 +1,36 @@
 package se.uog.model;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class UserType {
 
+    private final PropertyChangeSupport propertyChangeSupport;
     private UserEnum userEnum;
     private static UserType instance;
 
     private UserType(UserEnum userEnum){
         this.userEnum = userEnum;
+        this.propertyChangeSupport = new PropertyChangeSupport(this);
     }
 
     public UserEnum getUserEnum() {
         return userEnum;
     }
 
+    @Override
+    public String toString() {
+        return "UserType{" +
+            "userEnum=" + userEnum +
+            '}';
+    }
+
     public void setUserEnum(UserEnum userEnum) {
+        System.out.println("ffiring");
+        UserEnum oldNEnum = UserEnum.UNASSIGNED;
         this.userEnum = userEnum;
+        propertyChangeSupport.firePropertyChange("userEnum", oldNEnum, userEnum);
     }
 
     public static UserType getInstance() {
@@ -23,6 +39,15 @@ public class UserType {
         }
         return instance;
     }
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.removePropertyChangeListener(l);
+    }
+
 
 
     // Can make singleton?
